1a31c53148c8e31fba2aabf4cfde66b8e5643cef
Harshvardhan Singh <harshvardhansingh1613@gmail.com>
2024-03-04 17:58:55 +0530

    changes made by Harshit Sir

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,8 +1,34 @@
 <template>
-    <div class="">DashBoard</div>
+    <div class="container" :aria-busy="this.loading">
+        <h1>Dashboard</h1>
+        <div v-if="!loading">
+            <p v-if="this.user"> <img :src="this.user.photoURL" alt=""> Welcome {{user.displayName}}</p>
+        </div>
+        
+    </div>
 </template>
 <script>
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 export default{
-    name: "DashBoard"
+    name: "DashBoard",
+    data(){
+        return{
+            user: null,
+            loading: true
+        }
+    },
+    created(){
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                this.$router.push("/");
+            }
+            else{
+                this.user = user;
+                console.log(user)
+                this.loading = false;
+            }
+        });
+    }
 }
 </script>
