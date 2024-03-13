b8ce52f11640d237107d18c4a81cca222f59238d
Kritika Singh <kritikasingh00.com@gmail.com>
2024-03-13 16:02:14 +0530

    firestore

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAuhkWEZbsV4ThpoooWjtdWUfYMiWvWxng",
@@ -10,9 +11,14 @@ const firebaseConfig = {
   storageBucket: "enlit-fc989.appspot.com",
   messagingSenderId: "1089950344624",
   appId: "1:1089950344624:web:9b8f77c0477ced4601e275",
-  measurementId: "G-R08C6YX46M"
+  measurementId: "G-R08C6YX46M",
 };
 
-initializeApp(firebaseConfig);
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
 
-createApp(App).use(router).use(router).mount('#app')
+// Get Firestore instance
+const db = getFirestore(app);
+
+// Mount the Vue app
+createApp(App).use(router).provide("db", db).mount("#app");
--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,34 +1,69 @@
 <template>
-    <div class="container" :aria-busy="this.loading">
-        <h1>Dashboard</h1>
-        <div v-if="!loading">
-            <p v-if="this.user"> <img :src="this.user.photoURL" alt=""> Welcome {{user.displayName}}</p>
-        </div>
-        
+  <div class="container" :aria-busy="this.loading">
+    <div v-if="!loading">
+      <p v-if="this.user">Welcome {{ user.displayName }}</p>
     </div>
+
+    <nav style="justify-content: center">
+      <ul>
+        <li>
+          <a href="#" @click.prevent="showComponent('ProfileComp')">Profile</a>
+        </li>
+        <li>
+          <a href="#" @click.prevent="showComponent('RuleBook')">Rulebooks</a>
+        </li>
+        <li>
+          <a href="#" @click.prevent="showComponent('NoticeComp')">Notices</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div>
+      <component :is="selectedComponent"></component>
+    </div>
+  </div>
 </template>
 <script>
+import CompetitionSection from "./CompetitionSection.vue";
+import NoticeComp from "@/components/dasboard/NoticeComp.vue";
+import ProfileComp from "@/components/dasboard/ProfileComp.vue";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-export default{
-    name: "DashBoard",
-    data(){
-        return{
-            user: null,
-            loading: true
-        }
+export default {
+  name: "DashBoard",
+  data() {
+    return {
+      user: null,
+      loading: true,
+      selectedComponent: ProfileComp,
+    };
+  },
+  created() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        this.$router.push("/");
+      } else {
+        this.user = user;
+        this.loading = false;
+      }
+    });
+  },
+  methods: {
+    showComponent(componentName) {
+      switch (componentName) {
+        case "ProfileComp":
+          this.selectedComponent = ProfileComp;
+          break;
+        case "RuleBook":
+          this.selectedComponent = CompetitionSection;
+          break;
+        case "NoticeComp":
+          this.selectedComponent = NoticeComp;
+          break;
+        default:
+          this.selectedComponent = ProfileComp;
+      }
     },
-    created(){
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if (!user) {
-                this.$router.push("/");
-            }
-            else{
-                this.user = user;
-                console.log(user)
-                this.loading = false;
-            }
-        });
-    }
-}
+  },
+};
 </script>
